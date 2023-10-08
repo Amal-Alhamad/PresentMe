448801d9b794ac1a2f291a75a8920f032b6cacb2
Amal-Alhamad <amalhamad1419@gmail.com>
2023-10-08 17:50:23 +0300

    delete some comments

--- PresentMe/File.swift
+++ PresentMe/File.swift
@@ -9,41 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      /* VStack {
-            Text("title").font(.largeTitle)
-        Divider()
-            Text("Details").font(.title2)
-
-            
-        }.foregroundColor(.white)
-            .padding().background(Color(.blue))
-    .frame(maxWidth:.infinity).cornerRadius(15)
-        
-        HStack {
-            Text("first")
-        
-            Text("middle").underline()
-            Text("last")
-
-            
-        }
-        .padding().cornerRadius(15).border(Color.orange)
-        
-        
-        HStack {
-            Image("Riyadh").resizable().frame(width: 65, height: 65)
-            VStack(alignment: .leading, content: {
-                Text("Riyadh")
-                Text("this is riyadh")
-            })
-            Spacer()
-            Text("🇸🇦")
-            
-            
-
-            
-        }.padding()*/
-        
+      
         VStack {
             Image("photo").resizable().frame(height: 200 ).clipShape(Circle()).overlay(Circle().stroke(Color.blue, lineWidth: 10))
             
